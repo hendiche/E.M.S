5bbf72ebbcc92deedbf2901a22486be33cbb4ba4
Donne <donne.workemail@gmail.com>
2018-09-12 22:21:34 +0900

    TinyMCE Test

--- resources/views/Backend/header.blade.php
+++ resources/views/Backend/header.blade.php
@@ -8,6 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{config('app.name'), 'System'}}</title>
 
+    <!-- TinyMCE -->
+    <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
     <!-- Custom Theme Style -->
     <link href="{{ asset('/css/EntryCSS.css') }}" rel="stylesheet">
 </head>
