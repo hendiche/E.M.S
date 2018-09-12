c22aca6e85aa0677675563bf96ed17f69e1a122a
Donne <donne.workemail@gmail.com>
2018-09-12 21:00:24 +0900

    Sample Entry UI
    Basic UI for typing in entries done.

---
+++ resources/views/Backend/header.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{config('app.name'), 'System'}}</title>
+
+    <!-- Custom Theme Style -->
+    <link href="{{ asset('/css/EntryCSS.css') }}" rel="stylesheet">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
