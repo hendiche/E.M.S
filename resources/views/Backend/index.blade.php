4604eb94c90377467a08afc606211c6294718c92
Ning <wenning.me@gmail.com>
2018-09-13 10:43:17 +0900

    Back: Modify event forms

--- resources/views/Backend/index.blade.php
+++ resources/views/Backend/index.blade.php
@@ -1,6 +1,31 @@
 @extends('Backend.layout')
 @section('content')
 <div id="main_content" class="shadoweffect">  
+  <div class="container">
+    <form>
+      <div class="form-group">
+        <label for="exampleFormControlSelect1">Example select</label>
+        <select class="form-control" id="exampleFormControlSelect1">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      
+      <div class="form-group">
+        <div class="form-row">
+          <div class="col-6 mb-3">
+            <label for="start_date">Start Date</label>
+            <input type="date" class="form-control" id="start_date">
+          </div>
+          <div class="col-6 mb-3">
+            <label for="end_date">Last name</label>
+            <input type="date" class="form-control" id="end_date">
+          </div>
+        </div>
+      </div>
     
     <div class="title">Post Message:</div>
     <form novalidate method="POST" action="{{ route('submit')}}">
@@ -20,9 +45,40 @@
 
             <input type="submit" value="Send">
         </div>
-    </form>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" placeholder="Event Name">
+      </div>
+      <div class="form-group">
+        <label for="details">Details</label>
+        <textarea class="form-control" id="details" rows="5"></textarea>
+      </div>
 
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+          Facebook
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+          Google Calender
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+        <label class="form-check-label" for="defaultCheck1">
+          Slack
+        </label>
+      </div>
+      <div class="text-center">
+        <button type="submit" class="btn btn-primary mt-3 pr-5 pl-5">Post</button>
+      </div>
+    </form>
+  </div>
 </div>
+
 <!--I disabled because richtext doesn't seem to be needed -->
 <!--script>
   var editor_config = {
